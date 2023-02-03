90fb8d8e616551b546d53d932fe73337178aca14
Gaspar Mutti <gasparrobortellamutti@gmail.com>
2023-02-03 19:42:55 -0300

    productModel update

--- src/dao/models/products.model.js
+++ src/dao/models/products.model.js
@@ -3,14 +3,39 @@ import mongoose from "mongoose";
 const productsCollection = "products";
 
 const productSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  price: Number,
-  category: String,
-  thumbnails: Array,
-  status: Boolean,
-  code: String,
-  stock: Number,
+  title: {
+    type: String,
+    require: true,
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+  price: {
+    type: Number,
+    require: true,
+  },
+  category: {
+    type: String,
+    require: true,
+  },
+  thumbnails: {
+    type: Array,
+    require: true,
+  },
+  status: {
+    type: Boolean,
+    require: true,
+  },
+  code: {
+    type: String,
+    unique: true,
+    require: true,
+  },
+  stock: {
+    type: Number,
+    require: true,
+  },
 });
 
 export const productModel = mongoose.model(productsCollection, productSchema);
